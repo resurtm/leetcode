eeb4200435a75bb8bb4b9739f09b37fa3c33e34b
resurtm <resurtm@gmail.com>
2024-12-28 14:12:59 +0100

    Add 0934-shortest-bridge solution (fast good solution)

--- _09/0934-shortest-bridge/solution-0934/src/main.rs
+++ _09/0934-shortest-bridge/solution-0934/src/main.rs
@@ -1,7 +1,101 @@
 use std::collections::{HashSet, VecDeque};
 
 impl Solution {
-    pub fn shortest_bridge(grid: Vec<Vec<i32>>) -> i32 {
+    pub fn shortest_bridge(grid_inp: Vec<Vec<i32>>) -> i32 {
+        let mut grid = grid_inp.clone();
+        let w = grid.len() as i32;
+        let h = grid[0].len() as i32;
+        let mut res = i32::MAX;
+
+        'outer: for i in 0..w {
+            for j in 0..h {
+                if grid[i as usize][j as usize] == 1 {
+                    let island = Self::mark_first_island(&mut grid, w, h, i, j);
+                    res = Self::move_to_second_island(&mut grid, &island, w, h);
+                    break 'outer;
+                }
+            }
+        }
+
+        res
+    }
+
+    fn move_to_second_island(
+        grid: &mut Vec<Vec<i32>>,
+        island: &HashSet<(i32, i32)>,
+        w: i32,
+        h: i32,
+    ) -> i32 {
+        let mut q = VecDeque::new();
+        island.iter().for_each(|it| q.push_front(*it));
+
+        let mut num = 3;
+
+        while !q.is_empty() {
+            let mut q_next = VecDeque::new();
+
+            while !q.is_empty() {
+                if let Some((x, y)) = q.pop_front() {
+                    let its = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+                    let its: Vec<_> = its
+                        .iter()
+                        .filter(|(m, n)| *m >= 0 && *n >= 0 && *m < w && *n < h)
+                        .filter(|(m, n)| grid[*m as usize][*n as usize] <= 1)
+                        .collect();
+                    for (m, n) in its.iter() {
+                        if grid[*m as usize][*n as usize] == 1 {
+                            return num - 3;
+                        }
+                        q_next.push_back((*m, *n));
+                        grid[*m as usize][*n as usize] = num;
+                    }
+                }
+            }
+
+            q = q_next;
+            num += 1;
+        }
+
+        i32::MAX
+    }
+
+    fn mark_first_island(
+        grid: &mut Vec<Vec<i32>>,
+        w: i32,
+        h: i32,
+        i: i32,
+        j: i32,
+    ) -> HashSet<(i32, i32)> {
+        let mut island = HashSet::new();
+        island.insert((i, j));
+
+        let mut q = VecDeque::new();
+        q.push_front((i, j));
+
+        grid[i as usize][j as usize] = 2;
+
+        while !q.is_empty() {
+            if let Some((x, y)) = q.pop_front() {
+                let its = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+                let its: Vec<_> = its
+                    .iter()
+                    .filter(|(m, n)| *m >= 0 && *n >= 0 && *m < w && *n < h)
+                    .filter(|(m, n)| grid[*m as usize][*n as usize] == 1)
+                    .collect();
+                its.iter().for_each(|(m, n)| {
+                    island.insert((*m, *n));
+
+                    q.push_back((*m, *n));
+
+                    grid[*m as usize][*n as usize] = 2;
+                });
+            }
+        }
+
+        island
+    }
+
+    pub fn shortest_bridge_slow(grid: Vec<Vec<i32>>) -> i32 {
         let w = grid.len() as i32;
         let h = grid[0].len() as i32;
 
