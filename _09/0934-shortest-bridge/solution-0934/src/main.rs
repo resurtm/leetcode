7caacab9b0f575e9f7b782d6cfad19423e7816e0
resurtm <resurtm@gmail.com>
2024-12-28 14:14:12 +0100

    Add 0934-shortest-bridge solution (clean up)

--- _09/0934-shortest-bridge/solution-0934/src/main.rs
+++ _09/0934-shortest-bridge/solution-0934/src/main.rs
@@ -21,7 +21,7 @@ impl Solution {
     }
 
     fn move_to_second_island(
-        grid: &mut Vec<Vec<i32>>,
+        grid: &mut [Vec<i32>],
         island: &HashSet<(i32, i32)>,
         w: i32,
         h: i32,
@@ -60,7 +60,7 @@ impl Solution {
     }
 
     fn mark_first_island(
-        grid: &mut Vec<Vec<i32>>,
+        grid: &mut [Vec<i32>],
         w: i32,
         h: i32,
         i: i32,
@@ -93,105 +93,6 @@ impl Solution {
         }
 
         island
-    }
-
-    pub fn shortest_bridge_slow(grid: Vec<Vec<i32>>) -> i32 {
-        let w = grid.len() as i32;
-        let h = grid[0].len() as i32;
-
-        let mut viz = HashSet::new();
-        let mut island0 = HashSet::new();
-        let mut island1 = HashSet::new();
-
-        for i in 0..w {
-            for j in 0..h {
-                if grid[i as usize][j as usize] == 0 || viz.contains(&(i, j)) {
-                    continue;
-                }
-                let island = Self::find_island(&grid, i, j);
-                island.iter().for_each(|(x, y)| {
-                    viz.insert((*x, *y));
-                });
-                if island0.is_empty() {
-                    island0 = island.clone();
-                } else if island1.is_empty() {
-                    island1 = island.clone();
-                }
-            }
-        }
-
-        let mut dist = i32::MAX;
-        for (i, j) in island0.iter() {
-            Self::check_dist(w, h, &island0, &island1, *i, *j, &mut dist);
-        }
-        dist
-    }
-
-    fn check_dist(
-        w: i32,
-        h: i32,
-        island0: &HashSet<(i32, i32)>,
-        island1: &HashSet<(i32, i32)>,
-        i: i32,
-        j: i32,
-        dist: &mut i32,
-    ) {
-        let mut viz = HashSet::new();
-
-        let mut q = VecDeque::new();
-        q.push_front((i, j, 0));
-        viz.insert((i, j));
-
-        while !q.is_empty() {
-            if let Some((x, y, d)) = q.pop_front() {
-                let its = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
-                let its: Vec<_> = its
-                    .iter()
-                    .filter(|(m, n)| *m >= 0 && *n >= 0 && *m < w && *n < h)
-                    .filter(|&it| !island0.contains(it))
-                    .filter(|&it| !viz.contains(it))
-                    .collect();
-                its.iter().for_each(|(m, n)| {
-                    if island1.contains(&(*m, *n)) {
-                        if *dist > d {
-                            *dist = d;
-                        }
-                    } else {
-                        q.push_back((*m, *n, d + 1));
-                        viz.insert((*m, *n));
-                    }
-                });
-            }
-        }
-    }
-
-    fn find_island(grid: &[Vec<i32>], i: i32, j: i32) -> HashSet<(i32, i32)> {
-        let w = grid.len() as i32;
-        let h = grid[0].len() as i32;
-
-        let mut viz = HashSet::new();
-
-        let mut q = VecDeque::new();
-        q.push_front((i, j));
-        viz.insert((i, j));
-
-        while !q.is_empty() {
-            if let Some((x, y)) = q.pop_front() {
-                let its = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
-                let its: Vec<_> = its
-                    .iter()
-                    .filter(|(m, n)| *m >= 0 && *n >= 0 && *m < w && *n < h)
-                    .filter(|(m, n)| grid[*m as usize][*n as usize] == 1)
-                    .filter(|&it| !viz.contains(it))
-                    .collect();
-                its.iter().for_each(|(m, n)| {
-                    q.push_back((*m, *n));
-                    viz.insert((*m, *n));
-                });
-            }
-        }
-
-        viz
     }
 }
 
