e9523c45688657331ab108d3b8e24378ef5f7243
resurtm <resurtm@gmail.com>
2025-01-13 17:18:06 +0100

    Add 3223-minimum-length-of-string-after-operations solution (fast and final)

--- _32/3223-minimum-length-of-string-after-operations/solution-3223/src/main.rs
+++ _32/3223-minimum-length-of-string-after-operations/solution-3223/src/main.rs
@@ -1,53 +1,19 @@
+use std::collections::HashMap;
+
 impl Solution {
     #[allow(dead_code)]
     pub fn minimum_length(si: String) -> i32 {
-        let mut s = si.into_bytes();
-        loop {
-            let mut removed = false;
-            let mut idx = -1;
-            loop {
-                idx += 1;
-                let id = idx as usize;
-                if id == s.len() {
-                    break;
-                }
-                if id == 0 || id == s.len() - 1 {
-                    continue;
-                }
-                let it = s[id];
-
-                let mut l = usize::MAX;
-                for idl in 0..id {
-                    if s[idl] == it {
-                        l = idl;
-                        break;
-                    }
-                }
-                if l == usize::MAX {
-                    continue;
-                }
-
-                let mut r = usize::MAX;
-                for idr in (id + 1..s.len()).rev() {
-                    if s[idr] == it {
-                        r = idr;
-                        break;
-                    }
-                }
-                if r == usize::MAX {
-                    continue;
-                }
-
-                removed = true;
-                idx -= 2;
-                s.remove(l);
-                s.remove(r - 1);
-            }
-            if !removed {
-                break;
+        let mut fr = HashMap::new();
+        si.as_bytes().iter().for_each(|x| {
+            fr.entry(x).and_modify(|y| *y += 1).or_insert(1);
+        });
+        let mut res = 0;
+        for i in b'a'..=b'z' {
+            if let Some(&c) = fr.get(&i) {
+                res += if c % 2 == 0 { 2 } else { 1 };
             }
         }
-        s.len() as i32
+        res
     }
 }
 
