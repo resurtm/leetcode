b0f81716d0c91ccd549cc3e7a9103aaad49f31b3
resurtm <resurtm@gmail.com>
2025-01-18 09:55:28 +0100

    Add 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid solution (final)

--- _13/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solution-1368/src/main.rs
+++ _13/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solution-1368/src/main.rs
@@ -1,50 +1,68 @@
 use std::collections::VecDeque;
 
 impl Solution {
+    fn dfs(
+        p: (usize, usize),
+        grid: &[Vec<i32>],
+        q: &mut VecDeque<(usize, usize)>,
+        v: &mut [Vec<i32>],
+        k: i32,
+    ) {
+        if q.contains(&p) || v[p.0][p.1] != i32::MAX {
+            return;
+        }
+        q.push_back(p);
+        v[p.0][p.1] = k;
+        match grid[p.0][p.1] {
+            4 => {
+                if p.0 > 0 {
+                    Self::dfs((p.0 - 1, p.1), grid, q, v, k);
+                }
+            }
+            3 => {
+                if p.0 < grid.len() - 1 {
+                    Self::dfs((p.0 + 1, p.1), grid, q, v, k);
+                }
+            }
+            2 => {
+                if p.1 > 0 {
+                    Self::dfs((p.0, p.1 - 1), grid, q, v, k);
+                }
+            }
+            1 => {
+                if p.1 < grid[0].len() - 1 {
+                    Self::dfs((p.0, p.1 + 1), grid, q, v, k);
+                }
+            }
+            _ => {}
+        }
+    }
+
     #[allow(dead_code)]
     pub fn min_cost(grid: Vec<Vec<i32>>) -> i32 {
-        const RIGHT: i32 = 1;
-        const LEFT: i32 = 2;
-        const DOWN: i32 = 3;
-        const UP: i32 = 4;
-
-        let (m, n) = (grid.len() as i32, grid[0].len() as i32);
-
-        let mut q: VecDeque<(Vec<(i32, i32)>, i32)> = VecDeque::new();
-        q.push_back((vec![(0, 0)], 0));
-
-        let mut res = vec![];
-
+        let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+        let mut v = vec![vec![i32::MAX; grid[0].len()]; grid.len()];
+        let mut k = 0;
+        Self::dfs((0, 0), &grid, &mut q, &mut v, k);
         while !q.is_empty() {
-            if let Some(curr) = q.pop_front() {
-                let last = *curr.0.last().unwrap();
-                if last == (m - 1, n - 1) {
-                    res.push(curr.1);
-                    continue;
+            k += 1;
+            let qc = std::mem::take(&mut q);
+            qc.iter().for_each(|p| {
+                if p.0 > 0 && !qc.contains(&(p.0 - 1, p.1)) {
+                    Self::dfs((p.0 - 1, p.1), &grid, &mut q, &mut v, k);
                 }
-                [
-                    (last.0 - 1, last.1, UP),
-                    (last.0 + 1, last.1, DOWN),
-                    (last.0, last.1 - 1, LEFT),
-                    (last.0, last.1 + 1, RIGHT),
-                ]
-                .iter()
-                .filter(|(x, y, _)| *x >= 0 && *y >= 0 && *x < m && *y < n)
-                .filter(|(x, y, _)| !curr.0.contains(&(*x, *y)))
-                .for_each(|(x, y, d)| {
-                    let mut next = curr.clone();
-                    next.0.push((*x, *y));
-                    next.1 = if grid[last.0 as usize][last.1 as usize] == *d {
-                        curr.1
-                    } else {
-                        curr.1 + 1
-                    };
-                    q.push_back(next);
-                });
-            }
+                if p.0 < grid.len() - 1 && !qc.contains(&(p.0 + 1, p.1)) {
+                    Self::dfs((p.0 + 1, p.1), &grid, &mut q, &mut v, k);
+                }
+                if p.1 > 0 && !qc.contains(&(p.0, p.1 - 1)) {
+                    Self::dfs((p.0, p.1 - 1), &grid, &mut q, &mut v, k);
+                }
+                if p.1 < grid[0].len() - 1 && !qc.contains(&(p.0, p.1 + 1)) {
+                    Self::dfs((p.0, p.1 + 1), &grid, &mut q, &mut v, k);
+                }
+            });
         }
-
-        *res.iter().min().unwrap()
+        *v.last().unwrap().last().unwrap()
     }
 }
 
