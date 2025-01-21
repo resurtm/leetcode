21682fca1a54f26ee8292d9e20d23a6e852a8bf0
resurtm <resurtm@gmail.com>
2025-01-21 18:30:21 +0100

    Add 2017-grid-game solution (final)

--- _20/2017-grid-game/solution-2017/src/main.rs
+++ _20/2017-grid-game/solution-2017/src/main.rs
@@ -1,51 +1,17 @@
 impl Solution {
     #[allow(dead_code)]
     pub fn grid_game(grid: Vec<Vec<i32>>) -> i64 {
-        let mut grid = grid.clone();
-        let len = grid[0].len();
-
-        let (mut front, mut back) = (vec![0i64; len], vec![0i64; len]);
-        for i0 in 0..len {
-            front[i0] = if i0 == 0 { 0 } else { front[i0 - 1] } + grid[0][i0] as i64;
-            let i1 = len - i0 - 1;
-            back[i1] = if i1 == len - 1 { 0 } else { back[i1 + 1] } + grid[1][i1] as i64;
+        let (mut t, mut b, mut r) = (
+            grid[0].iter().fold(0i64, |acc, x| acc + *x as i64),
+            0,
+            i64::MAX,
+        );
+        for (i, j) in grid[0].iter().zip(grid[1].iter()) {
+            t -= *i as i64;
+            r = r.min(t.max(b));
+            b += *j as i64;
         }
-
-        let (mut imax, mut max) = (0, i64::MIN);
-        for i in 0..len {
-            let sum = front[i] + back[i];
-            if max < sum {
-                imax = i;
-                max = sum;
-            }
-        }
-
-        println!("{:?}", grid);
-        println!("{} {}", imax, max);
-
-        grid[0].splice(0..=imax, vec![0i32; imax + 1]);
-        grid[1].splice(imax..len, vec![0i32; len - imax]);
-
-        let (mut front, mut back) = (vec![0i64; len], vec![0i64; len]);
-        for i0 in 0..len {
-            front[i0] = if i0 == 0 { 0 } else { front[i0 - 1] } + grid[0][i0] as i64;
-            let i1 = len - i0 - 1;
-            back[i1] = if i1 == len - 1 { 0 } else { back[i1 + 1] } + grid[1][i1] as i64;
-        }
-
-        let (mut imax, mut max) = (0, i64::MIN);
-        for i in 0..len {
-            let sum = front[i] + back[i];
-            if max < sum {
-                imax = i;
-                max = sum;
-            }
-        }
-
-        println!("{:?}", grid);
-        println!("{} {}", imax, max);
-
-        max
+        r
     }
 }
 
