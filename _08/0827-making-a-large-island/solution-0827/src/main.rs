d0a7c1deafa4685b1b3253c08c0f7f1e1323ad9f
resurtm <resurtm@gmail.com>
2025-01-31 13:16:18 +0100

    Add 0827-making-a-large-island solution (final)

--- _08/0827-making-a-large-island/solution-0827/src/main.rs
+++ _08/0827-making-a-large-island/solution-0827/src/main.rs
@@ -4,90 +4,55 @@ impl Solution {
     #[allow(dead_code)]
     pub fn largest_island(grid: Vec<Vec<i32>>) -> i32 {
         let (w, h) = (grid.len() as i32, grid[0].len() as i32);
-        let mut neighbors = vec![vec![-2; h as usize]; w as usize];
-
+        let (mut islands, mut num_islands) = (vec![vec![(-1, -1); h as usize]; w as usize], 0);
         let mut viz: HashSet<(i32, i32)> = HashSet::new();
-        let mut islands = vec![];
-
-        let mut parents: HashMap<(i32, i32), (i32, i32)> = HashMap::new();
-        let mut sizes: HashMap<(i32, i32), i32> = HashMap::new();
+        let (mut has_zero, mut res) = (false, 0);
         for i in 0..w {
             for j in 0..h {
                 if grid[i as usize][j as usize] == 0 {
-                    neighbors[i as usize][j as usize] = -1;
+                    has_zero = true;
                     continue;
                 }
-                parents.insert((i, j), (i, j));
-                sizes.insert((i, j), 1);
-                if !viz.contains(&(i, j)) {
-                    let island = Self::bfs((i, j), &grid, &mut neighbors, islands.len() as i32);
-                    viz.extend(island.iter());
-                    islands.push(island);
+                if viz.contains(&(i, j)) {
+                    continue;
                 }
+                let (island, island_size) = Self::bfs((i, j), &grid, w, h);
+                for &(i, j) in island.iter() {
+                    islands[i as usize][j as usize] = (num_islands, island_size);
+                }
+                num_islands += 1;
+                viz.extend(island.iter());
+                res = res.max(island_size);
             }
         }
-
-        for island in islands.iter() {
-            let mut root = (-1, -1);
-            for &curr in island {
-                if root == (-1, -1) {
-                    root = curr;
-                } else {
-                    Self::unify(root, curr, &mut parents, &mut sizes);
-                }
-            }
+        if !has_zero {
+            return res;
         }
-
-        let mut res = *sizes.values().max().unwrap_or(&0);
-
-        let mut checked_dirs = vec![];
         for i in 0..w {
             for j in 0..h {
                 if grid[i as usize][j as usize] != 0 {
                     continue;
                 }
-                let dirs: HashSet<_> = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+                let neighbors: HashMap<i32, i32> = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
                     .iter()
                     .filter(|(m, n)| *m >= 0 && *n >= 0 && *m < w && *n < h)
-                    .map(|(m, n)| neighbors[*m as usize][*n as usize])
-                    .filter(|neighbor| *neighbor >= 0)
+                    .map(|(m, n)| islands[*m as usize][*n as usize])
+                    .filter(|(_, n)| *n > 0)
                     .collect();
-                if checked_dirs.contains(&dirs) {
-                    continue;
-                }
-                let (mut parents_cl, mut sizes_cl) = (parents.clone(), sizes.clone());
-                if !dirs.is_empty() {
-                    let main_dir = *islands[*dirs.iter().next().unwrap() as usize]
-                        .iter()
-                        .next()
-                        .unwrap();
-                    for &dir in dirs.iter() {
-                        Self::unify(
-                            main_dir,
-                            *islands[dir as usize].iter().next().unwrap(),
-                            &mut parents_cl,
-                            &mut sizes_cl,
-                        );
-                    }
-                }
-                res = res.max(*sizes_cl.values().max().unwrap_or(&0) + 1);
-                checked_dirs.push(dirs);
+                res = res.max(
+                    1 + neighbors.iter().fold(0, |mut acc, x| {
+                        acc += x.1;
+                        acc
+                    }),
+                );
             }
         }
-
         res
     }
 
-    fn bfs(
-        start: (i32, i32),
-        grid: &[Vec<i32>],
-        neighbors: &mut [Vec<i32>],
-        curr_island: i32,
-    ) -> HashSet<(i32, i32)> {
-        let (w, h) = (grid.len() as i32, grid[0].len() as i32);
+    fn bfs(start: (i32, i32), grid: &[Vec<i32>], w: i32, h: i32) -> (HashSet<(i32, i32)>, i32) {
         let mut queue = vec![start];
         let mut viz = HashSet::from([start]);
-        neighbors[start.0 as usize][start.1 as usize] = curr_island;
         while !queue.is_empty() {
             if let Some(curr) = queue.pop() {
                 let dirs: Vec<_> = [
@@ -105,41 +70,11 @@ impl Solution {
                 for &dir in dirs.iter() {
                     queue.push(dir);
                     viz.insert(dir);
-                    neighbors[dir.0 as usize][dir.1 as usize] = curr_island;
                 }
             }
         }
-        viz
-    }
-
-    fn find(to_find: (i32, i32), parents: &HashMap<(i32, i32), (i32, i32)>) -> (i32, i32) {
-        let mut curr = to_find;
-        while curr != parents[&curr] {
-            curr = parents[&curr];
-        }
-        curr
-    }
-
-    fn unify(
-        a: (i32, i32),
-        b: (i32, i32),
-        parents: &mut HashMap<(i32, i32), (i32, i32)>,
-        sizes: &mut HashMap<(i32, i32), i32>,
-    ) {
-        if a == b {
-            return;
-        }
-        let (root1, root2) = (Self::find(a, parents), Self::find(b, parents));
-        if root1 == root2 {
-            return;
-        }
-        if sizes[&root1] < sizes[&root2] {
-            sizes.insert(root2, sizes[&root1] + sizes[&root2]);
-            parents.insert(root1, root2);
-        } else {
-            sizes.insert(root1, sizes[&root1] + sizes[&root2]);
-            parents.insert(root2, root1);
-        }
+        let size = viz.len() as i32;
+        (viz, size)
     }
 }
 
@@ -182,5 +117,17 @@ mod tests {
     fn test_case5() {
         let actual = Solution::largest_island(vec![vec![0, 0], vec![0, 0]]);
         assert_eq!(actual, 1);
+    }
+
+    #[test]
+    fn test_case6() {
+        let actual = Solution::largest_island(vec![vec![0, 0], vec![1, 1]]);
+        assert_eq!(actual, 3);
+    }
+
+    #[test]
+    fn test_case7() {
+        let actual = Solution::largest_island(vec![vec![0, 1, 0], vec![1, 0, 1], vec![1, 0, 0]]);
+        assert_eq!(actual, 5);
     }
 }
